057c9564aa524d503de54166c84ff0b489beecad
Nadia Lee <miss.nadia.lee@gmail.com>
2018-08-29 20:18:14 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -17,3 +17,7 @@ const createFareMultiplier = function (int) {
 const fareDoubler = createFareMultiplier(2);
 
 const fareTripler = createFareMultiplier(3);
+
+const selectDifferentDrivers = function (drivers, eitherDriver) {
+  return eitherDriver(drivers);
+};
